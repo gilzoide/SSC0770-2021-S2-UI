fa62e5934ebda6b18b292c47469535bf181deaf4
gilzoide <gilzoide@gmail.com>
2021-11-21 15:15:51 -0300

    Rename Speed -> Velocidade

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    public Vector3 Speed = new Vector3(30, 30, 30);
+    public Vector3 Velocidade = new Vector3(30, 30, 30);
 
     void Update()
     {
-        transform.Rotate(Speed * Time.deltaTime);
+        transform.Rotate(Velocidade * Time.deltaTime);
     }
 }
